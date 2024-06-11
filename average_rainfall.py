a36173cb787d79e6719126e2efe7f00132d1cfe2
karla <karla.prudom@csuglobal.edu>
2024-06-11 18:23:00 -0500

    Module 5

--- average_rainfall.py
+++ average_rainfall.py
@@ -1,15 +1,32 @@
-num_years = int(input("Enter the number of years: "))
+#####
+#Part 1: Average Rainfall calculation
+#Pseudocode
+#1. Ask the user for the number of years to calculate rainfall for
+#2. Initialize total_rainfall and total_months variable
+#3. Create a month array
+#4. Create an outer loop to iterate through the years 
+#5. Create an inner loop to iterate each month
+#6. For every month as the user for the total_rainfall for the month
+#7. Calculate the total_months
+#8. Calculate the average_rainfall
+#9. Display the total number of months, total rainfall, and average rainfall per month
+#####
+#number of years to calculate
+num_years = int(input("Enter the number of years to calculate: "))
 
 total_rainfall = 0
 total_months = 0
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
+#loop through the number of years and then loop through the number of months in that year
 for year in range(1, num_years + 1):
-    print(f"Year {year}:")
-    for month in range(1, 13):
-        rainfall = float(input(f"Enter the inches of rainfall for month {month}: "))
+    print(f"<== Year {year} ==>")
+    for month in range(12):
+        rainfall = float(input(f"In year {year} enter the inches of rainfall for {months[month]}: "))
         total_rainfall += rainfall
     total_months += 12
 
+#calculate average rainfall
 average_rainfall = total_rainfall / total_months
 
 print(f"\nTotal number of months: {total_months}")
--- bookclub_points.py
+++ bookclub_points.py
@@ -1,4 +1,13 @@
-num_books = int(input("Enter the number of books purchased this month: "))
+#####
+#Part 2: Book Club
+#Pseudocode
+#1. Ask the user for the number of books they purchased this month.
+#2. Intialize points
+#3. Use if-elif-else statements to determine the points based on the number of books purchased.
+#4. Display the number of points awarded
+#####
+#number of books purchased in the month
+num_books = int(input("How many books did you purchase this month? "))
 
 if num_books == 0:
     points = 0
@@ -13,4 +22,5 @@ elif num_books >= 8:
 else:
     points = 0
 
-print(f"Number of points awarded: {points}")
+#points awarded for books purchased
+print(f"{points} points awarded")
